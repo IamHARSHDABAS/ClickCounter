6787196b9e290343daa75a34ac29cee9c520ebfa
Harsh Dabas <iamharshdabas@gmail.com>
2023-02-23 20:23:42 +0530

    Layout

--- app/src/main/java/com/iamharshdabas/clickcounter/MainActivity.kt
+++ app/src/main/java/com/iamharshdabas/clickcounter/MainActivity.kt
@@ -2,10 +2,19 @@ package com.iamharshdabas.clickcounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var count = 0
+        val textNumberOfClicks = findViewById<TextView>(R.id.textNumberOfClicks)
+        val buttonClick = findViewById<Button>(R.id.buttonClick)
+        buttonClick.setOnClickListener {
+            count++
+            textNumberOfClicks.text = "$count"
+        }
     }
 }
