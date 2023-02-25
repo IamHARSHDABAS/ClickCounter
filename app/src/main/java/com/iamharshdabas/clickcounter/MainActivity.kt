2ece79aa1847e3339da83f697d450cb123116548
IamHARSHDABAS <iamharshdabas@gmail.com>
2023-02-25 10:44:50 +0530

    Added Time

--- app/src/main/java/com/iamharshdabas/clickcounter/MainActivity.kt
+++ app/src/main/java/com/iamharshdabas/clickcounter/MainActivity.kt
@@ -4,19 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var count = 0
-        var time = false
         val textNumberOfClicks = findViewById<TextView>(R.id.textNumberOfClicks)
+        val textTime = findViewById<TextView>(R.id.textTime)
         val buttonClick = findViewById<Button>(R.id.buttonClick)
+        val timeStart = System.currentTimeMillis()
         buttonClick.setOnClickListener {
             count++
-            time = true
             textNumberOfClicks.text = "$count"
+            if (count > 9) {
+                textTime.text = "${System.currentTimeMillis() - timeStart}"
+                buttonClick.isEnabled = false
+            }
         }
     }
 }
