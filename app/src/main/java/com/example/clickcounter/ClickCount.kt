55ecc65c7012c345f1a873a6ff70f175d47b80a2
IamHARSHDABAS <iamharshdabas@gmail.com>
2023-02-28 19:32:33 +0530

    Better now

--- app/src/main/java/com/example/clickcounter/ClickCount.kt
+++ app/src/main/java/com/example/clickcounter/ClickCount.kt
@@ -9,19 +9,6 @@ class ClickCount : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_click_count)
-        var count = 0
-        val textNumberOfClicks = findViewById<TextView>(R.id.textNumberOfClicks)
-        val textTime = findViewById<TextView>(R.id.textTime)
-        val buttonClick = findViewById<Button>(R.id.buttonClick)
-        val timeStart = System.currentTimeMillis()
-        val clickValue = intent.getStringExtra("EXTRA_CLICK_VALUE") // TODO THIS GIVES NULL VALUE
-        buttonClick.setOnClickListener {
-            count++
-            textNumberOfClicks.text = "$count"
-            if (count > 9) {
-                textTime.text = "${(System.currentTimeMillis() - timeStart) / 1000}"
-                buttonClick.isEnabled = false
-            }
-        }
+
     }
 }
--- app/src/main/java/com/example/clickcounter/MainActivity.kt
+++ app/src/main/java/com/example/clickcounter/MainActivity.kt
@@ -1,22 +1,31 @@
 package com.example.clickcounter
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val editTextClickValue = findViewById<EditText>(R.id.editTextClickValue)
-        val buttonSubmit = findViewById<Button>(R.id.buttonSubmit)
-        buttonSubmit.setOnClickListener {
-            val clickValue = editTextClickValue.text.toString().toInt()
-            val intent = Intent(this, ClickCount::class.java)
-            intent.putExtra("EXTRA_CLICK_VALUE", clickValue)
-            startActivity(intent)
+        var count = 0
+        val textNumberOfClicks = findViewById<TextView>(R.id.textNumberOfClicks)
+        val textTime = findViewById<TextView>(R.id.textTime)
+        val buttonClick = findViewById<Button>(R.id.buttonClick)
+        val timeStart = System.currentTimeMillis()
+        val clickValue = intent.getStringExtra("EXTRA_CLICK_VALUE") // TODO THIS GIVES NULL VALUE
+        buttonClick.setOnClickListener {
+            count++
+            textNumberOfClicks.text = "$count"
+            if (count == 69) {
+                textTime.text = "${System.currentTimeMillis() - timeStart}"
+                buttonClick.isEnabled = false
+            }
         }
     }
 }
